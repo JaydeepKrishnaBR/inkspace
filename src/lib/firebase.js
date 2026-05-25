7badd9506c4da892b2694a8ed095fb0f5556dfb6
shreenidhibr13 <shreenidhibr2001@gmail.com>
2026-05-25 14:24:54 +0530

    updated ENV files

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore }           from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
-}
+  apiKey: "AIzaSyC1wcJp1Xdn0Kh3yBsqH6zQobJqF3esH6E",
+  authDomain: "inkspace-app.firebaseapp.com",
+  projectId: "inkspace-app",
+  storageBucket: "inkspace-app.firebasestorage.app",
+  messagingSenderId: "904175661287",
+  appId: "1:904175661287:web:b4553df03a83aff9ebb242"
+};
 
 const app = initializeApp(firebaseConfig)
 export const auth           = getAuth(app)
